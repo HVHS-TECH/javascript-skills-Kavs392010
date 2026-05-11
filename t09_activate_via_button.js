aac0a1b846c5834a9d7696a3ebe932664968bd07
Kavya3910 <24635kc@hvhs.school.nz>
2026-05-12 09:27:37 +1200

    task 9 completed

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -15,23 +15,24 @@ var futureAge= myAgeinNextfourMonths + 10;
 Main
 ****************************/
 
-function start(){
-console.log("Hi " + myName + ". As of " + newDate + " I am " + myAge + " years old.");
-console.log("I was born in " + birthYear + "." )
-console.log("In 10 years, I will be " + futureAge + " years old.");
-console.log("I have " + userMoney + " dollars in my bank account. ");
-console.log("I spend half of it, now I have " + userMoney/2 + " dollars left in my account. ");
-console.log("Then I get $3, now I have " + ((userMoney/2)+3) + " dollars in my account. ");
-
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML= "<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML += "<h2>Hello " + myName + "!</h2>";
-OUTPUT.innerHTML += "<h2>Second Paragraph line</h2>";
-OUTPUT.innerHTML += "<h2>Third Paragraph Line</h2>";
-welcome();
-displayProduct("Chocolate", 4);
-displayProduct("Chips", 3);
-displayProduct("Drink", 2.50);
+function start(){
+    console.log("Hi " + myName + ". As of " + newDate + " I am " + myAge + " years old.");
+    console.log("I was born in " + birthYear + "." )
+    console.log("In 10 years, I will be " + futureAge + " years old.");
+    console.log("I have " + userMoney + " dollars in my bank account. ");
+    console.log("I spend half of it, now I have " + userMoney/2 + " dollars left in my account. ");
+    console.log("Then I get $3, now I have " + ((userMoney/2)+3) + " dollars in my account. ");
+
+    
+    OUTPUT.innerHTML= "<h2>Added by JavaScript</h2>";
+    OUTPUT.innerHTML += "<h2>Hello " + myName + "!</h2>";
+    OUTPUT.innerHTML += "<h2>Second Paragraph line</h2>";
+    OUTPUT.innerHTML += "<h2>Third Paragraph Line</h2>";
+    welcome();
+    displayProduct("Chocolate", 4);
+    displayProduct("Chips", 3);
+    displayProduct("Drink", 2.50);
 }
 /****************************
 Functions
